baae2fd99aabb4afd6db6a46ea12005f63a81bba
Twoxbradley <jimmybradley32@gmail.com>
2018-08-09 17:42:49 -0700

    saving progress

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon and Beast');
+INSERT INTO burgers (name) VALUES ('Bleu Moo and Bacon');
+INSERT INTO burgers (name, sleepy) VALUES ('Moo Royal', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Flamin Heifer', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Moov Ova (Veggie Burger)', true);
+INSERT INTO burgers (name) VALUES ('Double Trouble');
